3c6b8a0bded8ef8adc208abdac230ea1ebff2f32
anders3408 <anders3408@yahoo.dk>
2013-12-22 02:16:45 +0100

    find5: ifdef changes

--- sound/soc/msm/qdsp6/q6afe.c
+++ sound/soc/msm/qdsp6/q6afe.c
@@ -74,11 +74,11 @@ static int32_t afe_callback(struct apr_client_data *data, void *priv)
 		/* payload[1] contains the error status for response */
 		if (payload[1] != 0) {
 			atomic_set(&this_afe.status, -1);
-/* OPPO 2013-04-24 huanggd Modify for reduce printk rate*/			
+#ifdef CONFIG_MACH_APQ8064_FIND5
 			if (printk_ratelimit())			
+#endif
 				pr_err("%s: cmd = 0x%x returned error = 0x%x\n",
 					__func__, payload[0], payload[1]);
-/* OPPO 2013-04-24 huanggd Modify end*/				
 		}
 		if (data->opcode == APR_BASIC_RSP_RESULT) {
 			switch (payload[0]) {
@@ -109,11 +109,11 @@ static int32_t afe_callback(struct apr_client_data *data, void *priv)
 				port_id = RT_PROXY_PORT_001_RX;
 				break;
 			default:
-/* OPPO 2013-04-24 huanggd Modify for reduce printk rate*/			
+#ifdef CONFIG_MACH_APQ8064_FIND5
 				if (printk_ratelimit())					
+#endif
 					pr_err("Unknown cmd 0x%x\n",
 						payload[0]);
-/* OPPO 2013-04-24 huanggd Modify end*/					
 				break;
 			}
 		} else if (data->opcode == AFE_EVENT_RT_PROXY_PORT_STATUS) {
