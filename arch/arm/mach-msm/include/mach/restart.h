CyanogenMod/android_kernel_oppo_n1

msm: restart: Add Oppo specific restart code

arch/arm/mach-msm/include/mach/restart.h
@@ -25,6 +25,17 @@
 #define SUB_UNKNOWN	0x0140
 #endif
 
+#ifdef CONFIG_MACH_OPPO
+#define FASTBOOT_MODE	0x77665500
+#define RECOVERY_MODE	0x77665502
+#define FACTORY_MODE	0x77665504
+#define WLAN_MODE	0x77665505
+#define RF_MODE		0x77665506
+#define KERNEL_MODE	0x7766550a
+#define MODEM_MODE	0x7766550b
+#define ANDROID_MODE	0x7766550c
+#endif
+
 #if defined(CONFIG_MSM_NATIVE_RESTART)
 void msm_set_restart_mode(int mode);
 void msm_restart(char mode, const char *cmd);

arch/arm/mach-msm/restart.c
@@ -66,7 +66,11 @@ static void *dload_mode_addr;
 
 /* Download mode master kill-switch */
 static int dload_set(const char *val, struct kernel_param *kp);
+#ifdef CONFIG_MACH_OPPO
+static int download_mode = 0;
+#else
 static int download_mode = 1;
+#endif
 module_param_call(download_mode, dload_set, param_get_int,
 			&download_mode, 0644);
 
@@ -266,6 +270,20 @@ void msm_restart(char mode, const char *cmd)
 			__raw_writel(0x77665500, restart_reason);
 		} else if (!strncmp(cmd, "recovery", 8)) {
 			__raw_writel(0x77665502, restart_reason);
+#ifdef CONFIG_MACH_OPPO
+		} else if (!strncmp(cmd, "rf", 2)) {
+			__raw_writel(RF_MODE, restart_reason);
+		} else if (!strncmp(cmd, "wlan", 4)) {
+			__raw_writel(WLAN_MODE, restart_reason);
+		} else if (!strncmp(cmd, "ftm", 3)) {
+			__raw_writel(FACTORY_MODE, restart_reason);
+		} else if (!strncmp(cmd, "kernel", 6)) {
+			__raw_writel(KERNEL_MODE, restart_reason);
+		} else if (!strncmp(cmd, "modem", 5)) {
+			__raw_writel(MODEM_MODE, restart_reason);
+		} else if (!strncmp(cmd, "android", 7)) {
+			__raw_writel(ANDROID_MODE, restart_reason);
+#endif
 		} else if (!strncmp(cmd, "oem-", 4)) {
 			unsigned long code;
 			code = simple_strtoul(cmd + 4, NULL, 16) & 0xff;
@@ -336,6 +354,9 @@ static int __init msm_restart_init(void)
 	msm_tmr0_base = msm_timer_get_timer0_base();
 	restart_reason = MSM_IMEM_BASE + RESTART_REASON_ADDR;
 	pm_power_off = msm_power_off;
+#ifdef CONFIG_MACH_OPPO
+	__raw_writel(KERNEL_MODE, restart_reason);
+#endif
 
 	return 0;
 }

virtvoid/kernel_qcom_cfx

arch/arm/mach-msm/include/mach/restart.h
@@ -25,6 +25,17 @@
 #define SUB_UNKNOWN	0x0140
 #endif
 
+#ifdef CONFIG_MACH_N1
+#define FASTBOOT_MODE	0x77665500
+#define RECOVERY_MODE	0x77665502
+#define FACTORY_MODE	0x77665504
+#define WLAN_MODE	0x77665505
+#define RF_MODE		0x77665506
+#define KERNEL_MODE	0x7766550a
+#define MODEM_MODE	0x7766550b
+#define ANDROID_MODE	0x7766550c
+#endif
+
 #if defined(CONFIG_MSM_NATIVE_RESTART)
 void msm_set_restart_mode(int mode);
 void msm_restart(char mode, const char *cmd);

arch/arm/mach-msm/restart.c
@@ -79,6 +79,10 @@ static int download_mode = 0;
 static int download_mode = 1;
 #endif
 
+#ifdef CONFIG_MACH_N1
+static int download_mode = 0;
+#endif
+
 module_param_call(download_mode, dload_set, param_get_int,
 			&download_mode, 0644);
 
@@ -320,6 +324,20 @@ void msm_restart(char mode, const char *cmd)
 			__raw_writel(0x77665500, restart_reason);
 		} else if (!strncmp(cmd, "recovery", 8)) {
 			__raw_writel(0x77665502, restart_reason);
+#ifdef CONFIG_MACH_N1
+		} else if (!strncmp(cmd, "rf", 2)) {
+			__raw_writel(RF_MODE, restart_reason);
+		} else if (!strncmp(cmd, "wlan", 4)) {
+			__raw_writel(WLAN_MODE, restart_reason);
+		} else if (!strncmp(cmd, "ftm", 3)) {
+			__raw_writel(FACTORY_MODE, restart_reason);
+		} else if (!strncmp(cmd, "kernel", 6)) {
+			__raw_writel(KERNEL_MODE, restart_reason);
+		} else if (!strncmp(cmd, "modem", 5)) {
+			__raw_writel(MODEM_MODE, restart_reason);
+		} else if (!strncmp(cmd, "android", 7)) {
+			__raw_writel(ANDROID_MODE, restart_reason);
+#endif
 		} else if (!strncmp(cmd, "oem-", 4)) {
 			unsigned long code;
 			code = simple_strtoul(cmd + 4, NULL, 16) & 0xff;
@@ -394,6 +412,9 @@ static int __init msm_restart_init(void)
 	__raw_writel(0x7766550a, restart_reason);
 #endif
 	pm_power_off = msm_power_off;
+#ifdef CONFIG_MACH_N1
+	__raw_writel(KERNEL_MODE, restart_reason);
+#endif
 
 	return 0;
 }