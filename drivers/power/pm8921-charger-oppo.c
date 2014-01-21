CyanogenMod/android_kernel_oppo_n1

Update from OPPO
1 fixed some issues with chargering, and battery
2 fixed some issues with earphone
3 fixed issue with camera, optimized the effect of camera.
4 optimzed driver of screen display and touch panel

Conflicts:
	arch/arm/configs/12025_debug_defconfig
	arch/arm/mach-msm/board-8064.c
	arch/arm/mm/dma-mapping.c
	drivers/input/touchscreen/Makefile
	drivers/media/video/msm/msm_mctl.c
	drivers/nfc/pn544.c
	drivers/power/pm8921-charger.c
	drivers/power/smb358_charger.c
	drivers/usb/otg/msm_otg.c
	drivers/video/msm/Makefile
	drivers/video/msm/lm3630_bl_909t.c
	drivers/video/msm/mdp4_overlay_dsi_cmd.c
	drivers/video/msm/mipi_dsi_host.c
	drivers/video/msm/mipi_orise_909t.c
	drivers/video/msm/mipi_orise_n1.c
	drivers/video/msm/msm_fb.c
	drivers/video/msm/msm_fb_panel.h
	kernel/power/wakelock.c
	sound/soc/codecs/wcd9310.c
	sound/soc/msm/apq8064.c

drivers/power/pm8921-charger-oppo.c
@@ -3941,10 +3941,18 @@ static irqreturn_t usbin_valid_irq_handler(int irq, void *data)
 	schedule_work(&the_chip->cancel_charge_det);
 #endif
 /*OPPO,Jiangsm add end*/
+/* OPPO 2013-11-06 sjc Modify begin for delay a exact time */
+#ifndef CONFIG_VENDOR_EDIT
 	if (usb_target_ma)
 		schedule_delayed_work(&the_chip->vin_collapse_check_work,
 				      round_jiffies_relative(msecs_to_jiffies
 						(VIN_MIN_COLLAPSE_CHECK_MS)));
+#else
+	if (usb_target_ma)
+		schedule_delayed_work(&the_chip->vin_collapse_check_work,
+				      msecs_to_jiffies(VIN_MIN_COLLAPSE_CHECK_MS));
+#endif
+/* OPPO 2013-11-06 sjc Modify end */
 	else
 	    handle_usb_insertion_removal(data);
 	return IRQ_HANDLED;

virtvoid/kernel_qcom_cfx

	arch/arm/mach-msm/socinfo.c
	drivers/input/touchscreen/synaptics_s3202_i2c_rmi_n1.c
	drivers/input/touchscreen/touch_y8c20x66a.c
	drivers/media/video/msm/oppo/m9mo_fw.h
	drivers/media/video/msm/oppo/m9mo_v4l2.c
	drivers/nfc/pn544-oppo.c
	drivers/video/msm/lm3630_bl.c
	drivers/video/msm/mdp4_overlay.c
	drivers/video/msm/mipi_dsi.c
	include/linux/boot_mode.h
	kernel/power/earlysuspend.c
	lib/ratelimit.c
	mm/backing-dev.c
	mm/vmscan.c
	sound/soc/codecs/wcd9310-oppo.c

drivers/power/pm8921-charger-oppo.c
@@ -3962,10 +3962,18 @@ static irqreturn_t usbin_valid_irq_handler(int irq, void *data)
 	schedule_work(&the_chip->cancel_charge_det);
 #endif
 /*OPPO,Jiangsm add end*/
+/* OPPO 2013-11-06 sjc Modify begin for delay a exact time */
+#ifndef CONFIG_MACH_N1
 	if (usb_target_ma)
 		schedule_delayed_work(&the_chip->vin_collapse_check_work,
 				      round_jiffies_relative(msecs_to_jiffies
 						(VIN_MIN_COLLAPSE_CHECK_MS)));
+#else
+	if (usb_target_ma)
+		schedule_delayed_work(&the_chip->vin_collapse_check_work,
+				      msecs_to_jiffies(VIN_MIN_COLLAPSE_CHECK_MS));
+#endif
+/* OPPO 2013-11-06 sjc Modify end */
 	else
 	    handle_usb_insertion_removal(data);
 	return IRQ_HANDLED;